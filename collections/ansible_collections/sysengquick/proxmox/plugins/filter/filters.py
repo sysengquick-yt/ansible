8239ade445b450710c6a0ca1ee1794f2e3f03e47
John Ratliff <john@technoplaza.net>
2024-05-11 23:37:35 +0000

    add checksum filter plugin

---
+++ collections/ansible_collections/sysengquick/proxmox/plugins/filter/filters.py
@@ -0,0 +1,45 @@
+# (c) 2024 John Ratliff <john@technoplaza.net>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)  # noqa: E501
+
+from __future__ import annotations
+
+import re
+
+from ansible.errors import AnsibleFilterError
+
+
+def checksum(checksum_file: str, filename: str) -> str:
+    try:
+        with open(checksum_file, "r") as f:
+            for line in f:
+                if match := re.match(
+                    r"SHA(256|512) \(" + filename + r"\) = ([a-f0-9]+)", line
+                ):
+                    return f"sha{match.group(1)}:{match.group(2)}"
+                if match := re.match(r"([0-9a-f]+) [ *]" + filename, line):
+                    checksum = match.group(1)
+                    length = len(checksum)
+
+                    if length == 64:
+                        return f"sha256:{checksum}"
+                    elif length == 128:
+                        return f"sha512:{checksum}"
+                    else:
+                        raise AnsibleFilterError(
+                            f"Unexpected checksumn length {length}. Expected 64 or 128."
+                        )
+    except IOError as e:
+        raise AnsibleFilterError(
+            f"Unable to read checksum data from {checksum_file}", orig_exc=e
+        )
+
+    raise AnsibleFilterError(
+        f"Unable to find checksum for {filename} in {checksum_file}"
+    )
+
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            "checksum": checksum,
+        }
